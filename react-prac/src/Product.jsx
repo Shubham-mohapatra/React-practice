9e6b14abcc7e0d7c45897d596af7c86ac7d713fd
Shubham Mohapatra <shubhammohapatra094@gmail.com>
2025-10-17 15:41:12 +0530

    React Props

--- react-prac/src/Product.jsx
+++ react-prac/src/Product.jsx
@@ -1,12 +1,23 @@
 import "./Product.css"
-function Product(){
-    return (
-        <div className="Product">
-            <h3> Product Title </h3>
-            <h5>Product Description </h5>
+function Product({title, price, features, additionalFeatures}){ // react props -> the information that are passed to a jsx tag 
+   // const list = features.map((feature, index) => <li key={index}>{feature}</li>) // use map method to render arrays 
+
+
+// Conditionals 
+    let styles = { backgroundColor :  price > 30000  ? "blue" : null
+    }   // we can add contions in the styles
+  return ( 
+        <div className="Product" style={styles}>
+            <h3>{title}</h3>
+            <p>Price: {price}</p>
+            <p>Features: {features.map((feature, index) => <li key={index}>{feature}</li>)}</p> 
+            <p>Discount</p> {price > 30000 && <p>"Discount of 5%"</p>}
+            <p>Additional Features: {additionalFeatures}</p>
         </div>
+
+     
     );
 }
 
-
+// we can use features.map using <li> </li>
 export default Product;
--- react-prac/src/ProductTab.jsx
+++ react-prac/src/ProductTab.jsx
@@ -1,9 +1,21 @@
 import Product from "./Product";
 function ProductTab(){
+   let feature =["hi-tech","durable","fast"];
+   let laptopFeatures = ["Intel i7", "16GB RAM", "SSD Storage"];
     return (
         <>
-        <Product/>
-        <Product/>
+        <Product 
+       title="iPhone 15" 
+       price={30000} 
+       features = {feature}
+       additionalFeatures="Face ID" 
+        />
+        <Product 
+        title="Laptop" 
+        price={100000}
+        features={laptopFeatures}
+        additionalFeatures="Gaming Ready"
+        />
         </>
     )
 }
